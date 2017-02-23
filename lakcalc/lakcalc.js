a1a0a4e8ac0daba7024d618a08b06c18f0e64819
Arno Unkrig <aunkrig@users.noreply.github.com>
2017-02-23 08:29:09 +0100

    Renamed function "kaempfe()" to "fight()".

--- lakcalc/lakcalc.js
+++ lakcalc/lakcalc.js
@@ -178,7 +178,7 @@ angular.module('myApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']).controller(
 
 	$scope.BUFF_OFF_BARRACKS = [ 1.01, 1.02, 1.02, 1.03, 1.04, 1.04, 1.05, 1.06, 1.06, 1.07, 1.08, 1.08, 1.09, 1.10, 1.10, 1.11, 1.12, 1.12, 1.13, 1.14, 1.14, 1.15, 1.16, 1.16, 1.17, 1.18, 1.18, 1.19, 1.19, 1.20 ];
 
-	$scope.kaempfe = function() {
+	$scope.fight = function() {
 
 		$scope.snapshot = [];
 
